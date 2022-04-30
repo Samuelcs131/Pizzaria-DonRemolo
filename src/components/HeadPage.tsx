a7bfd90cc36ef2c202924dd26d0a5cf295931b58
Samuel Claudino <samuelcs131@gmail.com>
2022-04-30 05:41:11 -0300

    ajuste cor cabecalho navegador

--- src/components/HeadPage.tsx
+++ src/components/HeadPage.tsx
@@ -1,11 +1,18 @@
 import Head from "next/head";
+import { useContext } from "react";
+import { DataContext } from "../store/GlobalState";
+import { iDataContext } from "../types/interfaces";
 import { tTitlePage } from "../types/types";
 
 
 const HeadPage = ({titlePage}: tTitlePage) => {
+    const { state } = useContext<iDataContext>(DataContext)
+ 
     return(
         <Head>
-            <title>{titlePage}</title> 
+            <title>{titlePage}</title>
+            <meta name="theme-color" 
+            content={state.themePage?.theme === 'light' ? 'white' : 'black'}/>
         </Head>
     )
 }
